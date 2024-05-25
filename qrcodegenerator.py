f4ed4820d745bfe0f8df95546ab09f39bd69a7f9
Nemitha Wijerathna <nemithan05@gmail.com>
2024-05-26 00:00:43 +0530

    generate qr code function

--- qrcodegenerator.py
+++ qrcodegenerator.py
@@ -9,6 +9,19 @@ def generate_qr_code():
     if data:
         qr = qrcode.QRCode(
             version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
             box_size=10,
             border=5
         )
+
+        qr.add_data(data)
+        qr.make(fit=True)
+
+        img = qr.make_image(fill_color="midnight", back_color="coral")
+
+        file_path = filedialog.asksaveasfilename(defaultextension=".png", filetypes=[("PNG files", "*.png")])
+
+        if file_path:
+            img.save(file_path)
+            messagebox.showinfo("QR Code generated successfully", f"QR Code saved at {file_path}")
+
